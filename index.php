22dc35e7053407577e6fd540c292244a86bc4fd5
Erick Lwanda <ericlwanda1@gmail.com>
2022-12-03 10:57:13 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,58 @@
 <?php
+
 // Read the variables sent via POST from our API
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
+$level = explode(“*”, $text);
+
+
+// // database name
+// $servername = "localhost";
+// $username = "id19949420_ussd";
+// $password = "PDAFk0?T9+Szhr";
+
+// // Create connection
+// $conn = new mysqli($servername, $username, $password);
+
+// // Check connection
+// if ($conn->connect_error) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $response  = "CON What would you want to check \n";
-    $response .= "1. My Account \n";
-    $response .= "2. My phone number";
-
-} else if ($text == "1") {
-    // Business logic for first level response
-    $response = "CON Choose account information you want to view \n";
-    $response .= "1. Account number \n";
-
-} else if ($text == "2") {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your phone number is ".$phoneNumber;
-
-} else if($text == "1*1") { 
-    // This is a second level response where the user selected 1 in the first instance
-    $accountNumber  = "ACC1001";
-
-    // This is a terminal request. Note how we start the response with END
-    $response = "END Your account number is ".$accountNumber;
+    $response  = "CON WELCOME SHULE POA \n";
+    $response .= "1. Registration \n";
+    $response .= "2. Primary \n";
+    $response .= "3. Secondary \n";
+    $response .= "4. College \n";
 
 }
 
-// Echo the response back to the API
-header('Content-type: text/plain');
-echo $response;
-?>
 
-<html>
-<h1>USSD API</h1>
-<p>Send a POST request to the following URL</p>
-<p>https://api.africastalking.com/version1/ussd/send</p>
-<p>with the following parameters</p>
-</html>
+if($level[0]=='1'){
+           if(isset($level[0]) && $level[0]!=”” && !isset($level[1])){
+        
+                $response="Enter Full name";
+            
+            }else if(isset($level[1]) && $level[1]!=”” && !isset($level[2])){
+                
+                $response="CON Hi".$level[1].",enter your age\n";
+                
+            } else if(isset($level[2]) && $level[2]!=”” && !isset($level[3])){
+                
+                $data=array(
+                'phonenumber'=>$phoneNumber,
+                'fullname' =>$level[1],
+                'age' => $level[2],
+                );
+                
+                $response="END Thank you ".$level[1]." for registering.\nWe will keep you updated";
+                // Echo the response back to the API
+                header('Content-type: text/plain');
+                echo $response;
+            }
+}
